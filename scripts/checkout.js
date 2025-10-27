1fe49349cb04a4b90ada50f04604de556092ffe2
Aderemi Oje <remoj2003@yahoo.com>
2025-10-27 08:21:56 +0100

    updated checkout.html

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
-// import {renderOrderSummary} from './checkout/OrderSummary.js';
-// import {renderPaymentSummary} from './checkout/paymentSummary.js';
-// import { renderCheckHeader } from './checkout/checkoutHeader.js';
+import {renderOrderSummary} from './checkout/OrderSummary.js';
+import {renderPaymentSummary} from './checkout/paymentSummary.js';
+import { renderCheckHeader } from './checkout/checkoutHeader.js';
 // import { loadProducts } from '../data/products.js';
 // import { loadProductsFetch } from '../data/products.js';
 // import '../data/cart-oop.js';
